2db330dab9e5d8377a324c06dd6a2e0ffab862e3
mcintyre321 <mcintyre321@gmail.com>
2012-01-20 16:40:03 +0000

    Added AttemptedValueHelper.cs

---
+++ WebNoodle/Helpers/AttemptedValueHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace WebNoodle.Helpers
+{
+    public static class AttemptedValueHelper
+    {
+        public static string AttemptedValue<T>(this ViewDataDictionary<T> vd, string name)
+        {
+            if (vd.ModelState.ContainsKey(name))
+            {
+                return vd.ModelState[name].Value.AttemptedValue;
+            }
+            return null;
+        }
+    }
+}
