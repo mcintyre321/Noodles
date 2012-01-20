4b57262bcae2bc29a864a74e2653935bc9d8cfc0
mcintyre321 <mcintyre321@gmail.com>
2012-01-20 12:41:17 +0000

    Re-added RenameableObjectMethodWrapper.cs

--- WebNoodle/Reflection/RenameableObjectMethodWrapper.cs
+++ WebNoodle/Reflection/RenameableObjectMethodWrapper.cs
@@ -2,44 +2,44 @@ using System.Collections.Generic;
 
 namespace WebNoodle.Reflection
 {
-    //public class RenameableObjectMethodWrapper : IObjectMethod
-    //{
-    //    private readonly IObjectMethod _inner;
-    //    private string _innerName;
-    //    private string _innerDisplayName;
+    public class RenameableObjectMethodWrapper : IObjectMethod
+    {
+        private readonly IObjectMethod _inner;
+        private string _innerName;
+        private string _innerDisplayName;
 
-    //    public string Name
-    //    {
-    //        get { return _innerName ?? _inner.Name; }
-    //        set { _innerName = value; }
-    //    }
+        public string Name
+        {
+            get { return _innerName ?? _inner.Name; }
+            set { _innerName = value; }
+        }
 
-    //    public string DisplayName
-    //    {
-    //        get { return _innerDisplayName ?? _inner.DisplayName; }
-    //        set { _innerDisplayName = value; }
-    //    }
+        public string DisplayName
+        {
+            get { return _innerDisplayName ?? _inner.DisplayName; }
+            set { _innerDisplayName = value; }
+        }
 
-    //    public IEnumerable<ObjectMethodParameter> Parameters
-    //    {
-    //        get { return _inner.Parameters; }
-    //    }
+        public IEnumerable<ObjectMethodParameter> Parameters
+        {
+            get { return _inner.Parameters; }
+        }
 
-    //    public IHasName Node
-    //    {
-    //        get { return _inner.Node; }
-    //    }
+        public object Target
+        {
+            get { return _inner.Target; }
+        }
 
-    //    public void Invoke(object[] parameters)
-    //    {
-    //        _inner.Invoke(parameters);
-    //    }
+        public void Invoke(object[] parameters)
+        {
+            _inner.Invoke(parameters);
+        }
 
-    //    public RenameableObjectMethodWrapper(IObjectMethod inner)
-    //    {
-    //        _inner = inner;
-    //    }
+        public RenameableObjectMethodWrapper(IObjectMethod inner)
+        {
+            _inner = inner;
+        }
 
-     
-    //}
+
+    }
 }
