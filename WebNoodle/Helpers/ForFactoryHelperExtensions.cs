34bf208320e8fc73d4aaa283516a12d1d6b98662
mcintyre321 <mcintyre321@gmail.com>
2012-03-03 17:23:40 +0000

    OKEY DOKEY

---
+++ WebNoodle/Helpers/ForFactoryHelperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using FormFactory;
+
+namespace WebNoodle.Helpers
+{
+    static class ForFactoryHelperExtensions
+    {
+        public static PropertyVm ToPropertyVm(this ObjectMethodParameter parameter, HtmlHelper Html)
+        {
+             return new PropertyVm(Html, parameter.ParameterType, parameter.Name, parameter.Id(), parameter.DisplayName)
+            {
+                GetCustomAttributes = () => parameter.CustomAttributes,
+                IsWritable = true,
+                Value = parameter.Value,
+                Choices = parameter.Choices,
+                Suggestions = parameter.Suggestions,
+                Source = parameter
+
+            };
+        }
+    }
+}
