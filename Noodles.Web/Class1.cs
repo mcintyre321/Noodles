5f25b109badbbcaab9d35f5b35a42f1edd440d90
Harry Mcintyre <mcintyre321@gmail.com>
2012-06-14 18:09:46 +0100

    Its the root, innit?

--- Noodles.Web/Class1.cs
+++ Noodles.Web/Class1.cs
@@ -12,7 +12,7 @@ namespace Noodles.Web
         public static T SetUrlRoot<T>(this T root, ControllerContext cc)
         {
             UrlHelper u = new UrlHelper(cc.RequestContext);
-            string url = u.Action(cc.RequestContext.RouteData.Values["action"] as string);
+            string url = u.Action(cc.RequestContext.RouteData.Values["action"] as string, cc.RequestContext.RouteData.Values["controller"] as string, new {path = ""});
             return root.SetUrlRoot(url);
         }
     
