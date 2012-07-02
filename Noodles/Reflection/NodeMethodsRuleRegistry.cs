3e6d05cc9907e2f18375a409967fae82ef4e3f2f
mcintyre321 <mcintyre321@gmail.com>
2012-07-02 13:45:02 +0100

    Tweak rule order

--- Noodles/Reflection/NodeMethodsRuleRegistry.cs
+++ Noodles/Reflection/NodeMethodsRuleRegistry.cs
@@ -26,17 +26,24 @@ namespace Noodles
         /// </returns>
         public delegate bool? ShowMethodRule(object target, MethodInfo methodInfo);
 
-        public static ShowMethodRule ShowAttributedMethods = (t, mi) => mi.GetCustomAttributes(typeof(ShowAttribute), true).Any() ? true : null as bool?;
         public static ShowMethodRule HideHideAttributedMethods = (t, mi) => mi.GetCustomAttributes(typeof(HideAttribute), true).Any() ? false : null as bool?;
         public static ShowMethodRule HideValuesMethods = (t, mi) => mi.Name.EndsWith("_values", StringComparison.InvariantCultureIgnoreCase) ? false : null as bool?; //choices should be hidden
         public static ShowMethodRule HideValueMethods = (t, mi) => mi.Name.ToLowerInvariant() != "set_value" && mi.Name.EndsWith("_value", StringComparison.InvariantCultureIgnoreCase) ? false : null as bool?; //choices should be hidden
         public static ShowMethodRule HideChoiceMethods = (t, mi) => mi.Name.EndsWith("_choices", StringComparison.InvariantCultureIgnoreCase) ? false : null as bool?; //choices should be hidden
-        public static ShowMethodRule HideSuggestionsMethods = (t, mi) => mi.Name.EndsWith("_suggestions", StringComparison.InvariantCultureIgnoreCase) ? false : null as bool?; //choices should be hidden
+        public static ShowMethodRule HideSuggestionsMethods = (t, mi) => mi.Name.EndsWith("_suggestions", StringComparison.InvariantCultureIgnoreCase) ? false : null as bool?; //suggestions should be hidden
         public static ShowMethodRule HideGetChildMethods = (t, mi) => (t is IGetChild && mi.Name == "Item") ? false : null as bool?;
         public static ShowMethodRule HideHasNodeMethods = (t, mi) => (t is IHasNodeMethods && mi.Name == "NodeMethods") ? false : null as bool?;
-        public static ShowMethodRule HideUndercoredMethods = (t, mi) => mi.Name.StartsWith("_") ? false : null as bool?;
+        public static ShowMethodRule HideUndercoredMethods = (t, mi) => ShowByDefault && mi.Name.StartsWith("_") ? false : null as bool?;
         public static ShowMethodRule HidePropertyGetters = (t, mi) => mi.Name.StartsWith("get_") ? false : null as bool?;
         public static ShowMethodRule HideSystemObjectMembers = (t, mi) => mi.DeclaringType == typeof(System.Object) ? false : null as bool?;
+        public static ShowMethodRule ShowAttributedMethods = (t, mi) =>
+        {
+            if (ShowByDefault == false && mi.GetCustomAttributes(typeof (ShowAttribute), true).Any())
+            {
+                return true;
+            }
+            return null as bool?;
+        };
         public static ShowMethodRule ClassLevelShowByDefault = (t, mi) => mi.DeclaringType.GetCustomAttributes(typeof(ShowAttribute), true).Any() ? true : null as bool?;
         public static ShowMethodRule ClassLevelHideByDefault = (t, mi) => mi.DeclaringType.GetCustomAttributes(typeof(HideAttribute), true).Any() ? false : null as bool?;
 
@@ -82,6 +89,7 @@ namespace Noodles
         }
 
         internal static U Maybe<T, U>(this T t, Func<T, U> f) where T : class
+        
         {
             return (t == null) ? default(U) : f(t);
         }
