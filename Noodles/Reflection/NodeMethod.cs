8b5c2c893783d5d9a8c38eb929615d15b77110a8
mcintyre321 <mcintyre321@gmail.com>
2012-07-23 23:53:32 +0100

    Removed all the guff for coercing parameters

--- Noodles/Reflection/NodeMethod.cs
+++ Noodles/Reflection/NodeMethod.cs
@@ -154,73 +154,19 @@ namespace Noodles
 
         private object GetParameterValue(object[] parameters, ParameterInfo parameterInfo, int index)
         {
-            if (index >= parameters.Length && parameterInfo.IsOptional) //looks there a new optional parameter has been added to the method
-            {
-                return null;
-            }
-
             var value = parameters[index];
 
             if (value == null) //the saved parameter was null
             {
                 return null;
             }
-            if (parameterInfo.ParameterType.IsAssignableFrom(value.GetType()))
-            {
-                return value;
-            }
-
-            {
-                var implicitConverter = ImplicitConversionMethodHelper.ImplicitConversionMethod(value.GetType(), parameterInfo.ParameterType);
-                if (implicitConverter != null)
-                {
-                    return implicitConverter.Invoke(null, new object[] { value });
-                }
-            }
-
-            if (value is DateTime && parameterInfo.ParameterType == typeof(DateTimeOffset))
-            {
-
-            }
-
-            //if (value.GetType() == typeof (JObject)) //maybe its a chunk of json
+            //if (!parameterInfo.ParameterType.IsAssignableFrom(value.GetType()))
             //{
-            //    var jsonObject = (JObject) value; //so deserialize as the expected type
-            //    var settings = new CustomSerializerSettings();
-            //    return JsonConvert.DeserializeObject(jsonObject.ToString(), parameterInfo.ParameterType, settings);
+            //    return value;
             //}
-            var underlyingType = (Nullable.GetUnderlyingType(parameterInfo.ParameterType) ?? parameterInfo.ParameterType);
-            if (underlyingType.IsEnum) //its an enum or a nullable enum
-            {
-                if (Nullable.GetUnderlyingType(parameterInfo.ParameterType) != null && string.IsNullOrWhiteSpace(value.ToString()))
-                {
-                    return null; //is nullable and is empty, so must be null
-                }
-
-                if (value as long? != null) //is it an int representation of an enum?
-                {
-                    return Enum.ToObject(underlyingType, (long)value);
-                }
-                return Enum.Parse(underlyingType, value.ToString());
-            }
-            return To(value, parameterInfo.ParameterType); //ok so they are different types... lets try a changetype
+            return value;
         }
-        static object To(object obj, Type t)
-        {
-            Type u = Nullable.GetUnderlyingType(t);
-            if (u != null)
-            {
-                if (obj == null)
-                    return null;
-
-                return Convert.ChangeType(obj, u);
-            }
-            else
-            {
-                return Convert.ChangeType(obj, t);
-            }
-        }
-
+       
 
         object IGetChild.this[string name]
         {
