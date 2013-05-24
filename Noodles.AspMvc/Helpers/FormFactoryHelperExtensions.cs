56359d6c5e2d3a0cab7066e03eb652fd69a05f16
Harry McIntyre <mcintyre321@gmail.com>
2013-05-24 19:17:48 +0100

    Added hidden

--- Noodles.AspMvc/Helpers/FormFactoryHelperExtensions.cs
+++ Noodles.AspMvc/Helpers/FormFactoryHelperExtensions.cs
@@ -8,6 +8,16 @@ namespace Noodles.AspMvc.Helpers
 {
     public static class FormFactoryHelperExtensions
     {
+        static object GetAttemptedValue(this HtmlHelper helper, string modelKey)
+        {
+            ModelState modelState;
+            if (helper.ViewData.ModelState.TryGetValue(modelKey, out modelState))
+            {
+                return modelState.Value.AttemptedValue;
+            }
+            return null;
+        }
+
         public static PropertyVm ToPropertyVm(this IInvokeableParameter parameter, HtmlHelper html)
         {
             var vm = new PropertyVm(html, parameter.ValueType, parameter.Name)
@@ -16,7 +26,7 @@ namespace Noodles.AspMvc.Helpers
                 GetCustomAttributes = () => parameter.CustomAttributes,
                 Readonly = false,
                 IsHidden = parameter.CustomAttributes.OfType<DataTypeAttribute>().Any(x => x.CustomDataType == "Hidden"),
-                Value = parameter.Value,
+                Value = parameter.LastValue,
                 Choices = parameter.Choices,
                 Suggestions = parameter.Suggestions,
                 Source = parameter,
@@ -25,9 +35,9 @@ namespace Noodles.AspMvc.Helpers
             return vm;
         }
          
+
         public static PropertyVm ToPropertyVm(this NodeProperty property, HtmlHelper html)
         {
-
             var vm = new PropertyVm(html, property.ValueType, property.Name)
             {
                 DisplayName = property.DisplayName,
