139edb027a61a26fc1af25650195f5f5802546b2
tim_iles <tim@timiles.com>
2013-05-31 16:11:07 +0100

    browser testing

---
+++ Noodles.Tests/Web/Features/NodeMethodsFeature.feature
@@ -0,0 +1,7 @@
+﻿Feature: NodeMethodsFeature
+
+@mytag
+Scenario: Click Node Method link button
+	Given I am on a page with a node method link
+	When I click the node method link
+	Then a node method form appears
---
+++ Noodles.Tests/Web/Steps/WebDriverExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Noodles.Tests.Web.Steps
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(drv => drv.FindElement(by));
+        }
+
+        public static ReadOnlyCollection<IWebElement> FindElements(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            wait.Until(drv => drv.FindElements(by).Any());
+            return driver.FindElements(by);
+        }
+
+        public static void WaitForReady(this IWebDriver driver, int timeoutInSeconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            ((IJavaScriptExecutor) driver).ExecuteScript("$(document).ready( function(){ $('body').addClass('jquery-ready'); } );");
+            driver.FindElement(By.CssSelector("body.jquery-ready"), 10);
+        }
+    }
+}
