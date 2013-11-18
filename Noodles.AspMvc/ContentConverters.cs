4ece6db39308b7502464640c9c62d3405ead2074
tim_iles <tim@timiles.com>
2013-11-18 16:11:46 +0000

    in/out fail

--- Noodles.AspMvc/ContentConverters.cs
+++ Noodles.AspMvc/ContentConverters.cs
@@ -6,12 +6,12 @@ namespace Noodles.AspMvc
 {
     public static class ContentConverters
     {
-        static readonly Dictionary<string, Func<object, INode>> Converters = new Dictionary<string, Func<object, INode>>();
-        public static Func<object, INode> Get(string targetType)
+        static readonly Dictionary<string, Func<INode, object>> Converters = new Dictionary<string, Func<INode, object>>();
+        public static Func<INode, object> Get(string targetType)
         {
             return Converters.ContainsKey(targetType) ? Converters[targetType] : null;
         }
-        public static void Set(string targetType, Func<object, INode> converter)
+        public static void Set(string targetType, Func<INode, object> converter)
         {
             Converters[targetType] = converter;
         }
